b84ae36589ac7e901ba7d72b1efb81c47f3c630a
Desktop-01\Ondrej <Ondrej.vinco5@gmail.com>
2021-02-05 23:19:27 +0100

    Intial commit of the first game

---
+++ MyFirstGame/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rB;
+    // Start is called before the first frame update
+    public float forwardForce = 20000f;
+    public float sideWayForce = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rB.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d")) {
+            rB.AddForce(sideWayForce * Time.deltaTime, 0,0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rB.AddForce(-sideWayForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
